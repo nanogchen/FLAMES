61a6b406f9f2b8df4e53b63c82ccf399d2b687c0
nanogchen-ibupower <nanogchen@ibuypower>
2026-06-11 07:25:38 -0500

    updated srcs

--- flames/calc.py
+++ flames/calc.py
@@ -25,8 +25,7 @@ from scipy.signal import correlate
 
 import sys
 sys.path.insert(0, "../")
-from flames.q_gen import get_rho_q,get_rho_q_noFF,get_q_points_all_quads,get_binning_averages,get_binning_averages_ttc
-from flames.q_gen import get_q_points_plane,get_q_points_angular_bin
+from flames.q_gen import get_rho_q,get_rho_q_noFF,get_q_points_all_quads,get_binning_averages
 
 def get_static_sf(q_points, system, traj, formfact_all):
 
@@ -47,90 +46,6 @@ def get_static_sf(q_points, system, traj, formfact_all):
 		ifr+=1
 
 	return ssf/(np.sum(formfact_all**2))
-
-def get_sf_decomposition(q_points, ag1, ag2, traj):
-	"""
-	get decomposition of the static structure factor
-	note: no manual-set form factor is used here!
-	"""
-
-	n_qpoints = len(q_points)
-	sf_AA = np.zeros((n_qpoints, len(traj)))
-	sf_AB = np.zeros((n_qpoints, len(traj)))
-	sf_BB = np.zeros((n_qpoints, len(traj)))    
-
-	ifr=0
-	for _ in traj:
-
-		coords_A = ag1.positions
-		coords_B = ag2.positions
-
-		rho_qA = get_rho_q_noFF(coords_A, q_points)
-		rho_qB = get_rho_q_noFF(coords_B, q_points)
-
-		sf_AA[:,ifr] = np.real(rho_qA*rho_qA.conjugate())
-		sf_BB[:,ifr] = np.real(rho_qB*rho_qB.conjugate())
-		sf_AB[:,ifr] = np.real(rho_qA*rho_qB.conjugate())+np.real(rho_qB*rho_qA.conjugate()) # considered two
-
-		ifr+=1
-
-	Natoms = ag1.atoms.n_atoms + ag2.atoms.n_atoms
-	return sf_AA/Natoms, 0.5*sf_AB/Natoms, sf_BB/Natoms
-
-def get_scattering_image(box, q_max, system, traj, plane='xz'):
-	"""
-	construct q-points in a plane
-	"""
-
-	q1,q2,q_points = get_q_points_plane(box, q_max, plane)
-
-	# out array
-	ssf_1d = np.zeros((len(q_points), len(traj)))
-	ssf_2d = np.zeros((q1.shape[0], q2.shape[0], len(traj)))
-
-	ifr=0    
-	for _ in traj:
-
-		coords = system.positions
-	
-		# cal sf. at each q-points
-		rho_q = get_rho_q_noFF(coords, q_points)
-		ssf = np.real(rho_q*rho_q.conjugate()) / coords.shape[0] # 1/N
-		ssf_1d[:, ifr] = ssf
-		ssf_2d[:, :, ifr] = np.reshape(ssf, (q1.shape[0], q2.shape[0]))
-
-	return q_points, ssf_1d, q1, q2, ssf_2d
-
-def get_ttc(box, q_min, q_max, Nbins, angle_deg, system, traj, formfact_all, plane='xz'):
-	"""
-	get two-time correlation C(q,t1,t2).
-	"""		
-
-	# generate q-points
-	q_points = get_q_points_angular_bin(box, q_min, q_max, Nbins, angle_deg, plane)
-
-	# first get s(q,t)
-	n_qpoints = len(q_points)
-	n_frs = len(traj)
-	ssf = np.zeros((n_qpoints, n_frs))
-
-	# get S(q,t)
-	ifr=0    
-	for _ in traj:
-
-		coords = system.positions
-		rho_q = get_rho_q(coords, q_points, formfact_all)
-		sq_t = np.real(rho_q*rho_q.conjugate()) 
-
-		ssf[:,ifr] = sq_t
-		ifr+=1
-
-	# get C(q, t1, t2) = <I1*I2>
-	I_q_t1_t2 = np.zeros((n_qpoints, n_frs, n_frs))
-	for iq in prange(n_qpoints):
-		I_q_t1_t2[iq] = np.outer(ssf[iq],ssf[iq])
-	
-	return q_points, ssf, I_q_t1_t2
 
 def get_ISF_corr(q_points, system, traj, formfact_all):
 
@@ -229,43 +144,3 @@ def binning_local(data_in_q_t, q_points):
 		averaged_data[bin_index, :] = data_bin
 
 	return q_bincenters, averaged_data
-
-def symm_func(t, inverse=True):
-	"""symmetric a sequence"""
-
-	if inverse:
-		t_ = t[::-1]*(-1)
-	else:
-		t_ = t[::-1]
-	t_ = list(t_[:-1])
-
-	t_.extend(list(t))
-
-	return np.array(t_)
-
-def fourier_transform_1d(x, fx):
-	x0, dx = x[0], x[1] - x[0]
-	g = np.fft.fft(fx) # DFT calculation
-	
-	# frequency normalization factor is 2*np.pi/dt
-	w = np.fft.fftfreq(x.size)*2*np.pi/dx # angular frequency
-	
-	# Multiply by external factor
-	g *= dx*np.exp(-complex(0,1)*w*x0) 
-	
-	return w,g
-
-def fft_dft_symm(Tseq, fTseq):
-	"""do Fourier tranformation on the autocorrelation functions, e.g, vel-acf using self-code
-	This gives the same results as the Filon formula
-	"""
-
-	# symmetrize the input
-	T_symm = symm_func(Tseq)
-	fT_symm = symm_func(fTseq, inverse=False)
-
-	# do FT
-	w, cw = fourier_transform_1d(T_symm, fT_symm)
-
-	return w[:len(w)//2], np.abs(cw)[:len(w)//2]
-	# return w, cw
